fcc3ee52347ebe5564c4e49b0fae3d2ae378e7f8
DanyDolphin <danielcastillo59814@ciencias.unam.mx>
2023-07-31 17:34:26 -0600

    Receiving email from API gateway

--- lambda_function.py
+++ lambda_function.py
@@ -10,12 +10,18 @@ from clients.gmail import GmailClient
 
 
 def lambda_handler(event, context):
+    if event['httpMethod'] == 'POST':
+        body = json.loads(event['body'])
+        recipient = body['email']
+    elif event['httpMethod'] == 'GET':
+        recipient = event['queryStringParameters']['email']
+
     report = Report()
     report.load_from_csv('transactions.csv')
     GmailClient().send_email(
         'Your transactions report at Stori',
         report.get_report(format='html'),
-        [sys.argv[1]])
+        [recipient])
     return {
         'statusCode': 200,
         'body': json.dumps('Mail enviado')}
